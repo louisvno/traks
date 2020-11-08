ae3fcee1bd74873a04b3bc666b4687b91b4d8c60
louisvno <louisvn@gmail.com>
2020-11-08 21:52:56 +0100

    open track from urrrl

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,11 @@
+import { AppComponent } from './app.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: ":id", component : AppComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/layer.service.ts
+++ src/app/layer.service.ts
@@ -2,11 +2,12 @@ import { TrackInfoControlService } from './track-info/track-info-control.service
 import { TrackService } from './track.service';
 import { MapService } from './map.service';
 import { Injectable } from '@angular/core';
-import { withLatestFrom, delay } from 'rxjs/operators';
-import { Subscription, Subject } from 'rxjs';
+import { withLatestFrom, delay, filter } from 'rxjs/operators';
+import { Subscription, Subject, Observable, combineLatest, zip } from 'rxjs';
 import { Set } from 'immutable'
 import { Track, TrackViewModel } from './model/TrackMetaData.model';
 import * as L from 'leaflet';
+import { RouterEvent, NavigationEnd, Router} from '@angular/router';
 
 @Injectable({
   providedIn: 'root',
@@ -19,26 +20,28 @@ export class LayerService {
   public trackSelected: Subject<TrackViewModel> = new Subject();
   private trkList: Subscription;
   public unfocusEvent = new Subject<boolean>();
+  public mapAndTracksLoaded = new Subject<boolean>();
+  private navigationEnd: Observable<RouterEvent>;
 
-  constructor(private mapService: MapService, private trackService:TrackService, private trackControls: TrackInfoControlService) { 
+  constructor(
+    private mapService: MapService,
+    private trackService:TrackService,
+    private trackControls: TrackInfoControlService,
+    private router : Router
+  ) { 
       this.trkList = this.trackService.trackList.pipe(
         withLatestFrom(this.mapService.map)
       )
       .subscribe(
         ([trks, map])=> {
-          console.log('hii')
-          trks.forEach(trk => {
-            const viewModel = this.polyLineFromTrack(trk);
-            if(!this.allLayers.has(viewModel)) this.allLayers.add(viewModel);
-            this.addToMap(viewModel.mapFeature,map);
-            this.addToMap(viewModel.touchHelper,map);
-          })
+          trks.forEach(trk =>  this.addTrack(trk, map));
+          this.mapAndTracksLoaded.next(true);
         }
       )
 
       this.trackSelected.pipe(
         withLatestFrom(mapService.map),
-        delay(100)
+        delay(100) // delay needed to wait for resize transform.
       )
       .subscribe(([trk, map]) => {
         this.focusOnTrack(trk.mapFeature,map)
@@ -46,11 +49,37 @@ export class LayerService {
 
       this.trackControls.closeTrackInfo.pipe(
         withLatestFrom(mapService.map),
-        delay(100)
+        delay(100) // delay needed to wait for resize transform.
       )
       .subscribe(([_,map]) => {
         this.unFocusTrack(map)
       })
+      
+      this.navigationEnd = this.router.events.pipe(
+        filter((event: RouterEvent) => event instanceof NavigationEnd));
+
+      combineLatest(this.navigationEnd, this.mapAndTracksLoaded)
+      .subscribe((([event, _]: [RouterEvent, boolean]) => {
+        console.log(event.url.slice(1));
+        console.log(_)
+        console.log(this.allLayers)
+        const track = this.allLayers.find(
+                              vm => { 
+                                return vm.model.fileName === event.url.slice(1)
+                              })
+        this.trackSelected.next(track);
+        }))
+  }
+
+  private addTrack(trk: Track, map: L.Map) {
+    const viewModel = this.polyLineFromTrack(trk);
+    if (!this.allLayers.has(viewModel)){
+      console.log("hii")
+      this.allLayers = this.allLayers.add(viewModel);
+    }
+    console.log(this.allLayers)
+    this.addToMap(viewModel.mapFeature, map);
+    this.addToMap(viewModel.touchHelper, map);
   }
 
   public async focusOnTrack(l : L.Polyline, map: L.Map){
@@ -69,8 +98,6 @@ export class LayerService {
     const endMarker = new L.Marker(latLngs[latLngs.length -1],{icon: endIcon});
     endMarker.addTo(map);
     this.markers.push(endMarker)
-    //show other track opacity 0.5?
-    //
   }
 
   public unFocusTrack(map: L.Map){
@@ -79,7 +106,7 @@ export class LayerService {
     this.unfocusEvent.next(true);
   }
 
-  public async addToMap(l: L.Layer, map: L.Map){
+  public addToMap(l: L.Layer, map: L.Map){
     if(!map.hasLayer(l)) l.addTo(map);
   }
 
@@ -90,8 +117,7 @@ export class LayerService {
 
     polylineHelper.on('click', (event: L.LeafletEvent) => {
       this.trackSelected.next({model: track , mapFeature: polyline, touchHelper: polylineHelper});
-    })
-    
+    })  
     return {model: track , mapFeature: polyline,touchHelper: polylineHelper};
   }
 
