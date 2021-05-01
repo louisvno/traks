4fcd617d702e736996923c62ae4e2676ce36d8d6
louisvno <louisvannoorden@gmail.com>
2021-05-01 12:02:28 +0200

    reset tab index on trackchange

--- src/app/component/track-info/track-info.component.ts
+++ src/app/component/track-info/track-info.component.ts
@@ -2,21 +2,37 @@ import { TrackInfoControlService } from './track-info-control.service';
 import { saveAs } from 'file-saver';
 import { Track } from '../../model/TrackMetaData.model';
 import { HttpClient } from '@angular/common/http';
-import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Component, Input, Output, EventEmitter, AfterViewInit, ViewChild, OnDestroy } from '@angular/core';
+import { LayerService } from 'src/app/service/layer.service';
+import { MatTabGroup } from '@angular/material/tabs';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-track-info',
   templateUrl: './track-info.component.html',
   styleUrls: ['./track-info.component.scss']
 })
-export class TrackInfoComponent implements OnInit {
+export class TrackInfoComponent implements AfterViewInit, OnDestroy {
 
-  constructor(private _http: HttpClient, private controls: TrackInfoControlService) { }
+  @ViewChild(MatTabGroup) tabGroup: MatTabGroup;
 
   @Input() public trackModel: Track;
   @Output() public onClose = new EventEmitter<boolean>();
 
-  ngOnInit(): void {
+  private resetTabOnTrackChange: Subscription;
+
+  constructor(private _http: HttpClient, private controls: TrackInfoControlService, private layerService: LayerService) { }
+  
+  ngOnDestroy(): void {
+    if(this.resetTabOnTrackChange) {
+      this.resetTabOnTrackChange.unsubscribe();
+    }
+  }
+
+  ngAfterViewInit(): void {
+    this.resetTabOnTrackChange = this.layerService.trackSelected.subscribe(res => {
+      this.tabGroup.selectedIndex = 0;
+    }); 
   }
 
   close(){
